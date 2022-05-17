4da198df6b1c21ed28fa767ddaf96e27f8f120f2
Ahhhhhhyeong <dkdud081@gmail.com>
2022-05-17 15:33:05 +0900

    Insert, Delete Test

---
+++ jdbc-practices/src/main/java/test/DeleteTest01.java
@@ -0,0 +1,58 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteTest01 {
+
+	public static void main(String[] args) {
+		delete((long) 5);
+		delete((long) 6);
+		delete((long) 7);
+	}
+
+	private static boolean delete(Long no) {
+		// 1. JDBC Driver 로딩(JDBC 클래스 로딩: class Loader)
+				boolean result = false;
+				Connection connection = null;
+				Statement stat = null; 
+				try {
+						Class.forName("org.mariadb.jdbc.Driver"); // static에서 커넥션을 만듦
+									
+						// 2. 연결하기
+						String url = "jdbc:mysql://127.0.0.1:3307/webdb?charset=utf8";
+						connection = DriverManager.getConnection(url, "webdb", "webdb");
+
+						// 3. Statement 생성
+						stat = connection.createStatement();
+							
+						// 4. SQL 실행
+						String sql = "delete from department where no = " + no;
+						int count = stat.executeUpdate(sql);					
+						result = count == 1;
+					} catch (ClassNotFoundException e) {
+						System.out.println("---FAIL---Driver Load " + e);
+					}catch (SQLException e) {
+						System.out.println("---FAIL---Driver Connected " + e);
+					}finally {			
+						try {
+							if(stat != null) {
+								stat.close();
+							}
+							if(connection != null) {
+								connection.close();
+							}
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+						}
+						
+					return result;
+		
+	}
+	
+	
+
+}
---
+++ jdbc-practices/src/main/java/test/InsertTest01.java
@@ -0,0 +1,57 @@
+package test;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class InsertTest01 {
+	public static void main(String[] args) {
+		insert("cs");
+		insert("경영지원팀");
+		insert("인프라");
+	}
+	
+	
+	public static boolean insert(String name) {
+		// 1. JDBC Driver 로딩(JDBC 클래스 로딩: class Loader)
+		boolean result = false;
+		Connection connection = null;
+		Statement stat = null; 
+		try {
+				Class.forName("org.mariadb.jdbc.Driver"); // static에서 커넥션을 만듦
+							
+				// 2. 연결하기
+				String url = "jdbc:mysql://127.0.0.1:3307/webdb?charset=utf8";
+				connection = DriverManager.getConnection(url, "webdb", "webdb");
+
+				// 3. Statement 생성
+				stat = connection.createStatement();
+					
+				// 4. SQL 실행
+				String sql = "INSERT INTO department values (null, '"+ name +"')";
+				int count = stat.executeUpdate(sql);					
+				result = count == 1;
+			} catch (ClassNotFoundException e) {
+				System.out.println("---FAIL---Driver Load " + e);
+			}catch (SQLException e) {
+				System.out.println("---FAIL---Driver Connected " + e);
+			}finally {			
+				try {
+					if(stat != null) {
+						stat.close();
+					}
+					if(connection != null) {
+						connection.close();
+					}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			return result;
+	}
+	
+	
+
+}
